914125f7141ba22a406b1fea028b59a8013ea1c3
jaykalathiya123 <kalathiyajay020@gmail.com>
2024-09-20 12:10:32 +0530

    Crud-from

---
+++ src/componets/employ-from/Employ.jsx
@@ -0,0 +1,102 @@
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Table from 'react-bootstrap/Table';
+import { useState } from 'react';
+
+function FromEmploy () {
+ const[fromData ,setFromDats]= useState()
+
+
+
+  return (
+    <>
+        <Form>
+      <Row className="mb-3">
+      <Form.Group className="mb-3" controlId="Frst Name">
+        <Form.Label>Frst Name</Form.Label>
+        <Form.Control type="text" placeholder="Frst Name" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="lest Name">
+        <Form.Label>lest Name</Form.Label>
+        <Form.Control type="text" placeholder="last Name" />
+      </Form.Group>
+        
+        <Form.Group as={Col} controlId="formGridEmail">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="1234 Main St" />
+      </Form.Group>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
+      </Row>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+      <Table responsive="md">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Table heading</th>
+            <th>Table heading</th>
+            <th>Table heading</th>
+            <th>Table heading</th>
+            <th>Table heading</th>
+            <th>Table heading</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+            <td>Table cell</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+
+
+
+
+  );
+}
+
+export default  FromEmploy ;
